b2d7a1b033cf5d3eb09030e598c1c00b9485666f
wbc0301 <417780894@qq.com>
2020-02-12 15:35:24 +0800

    Update main.js
    优化

--- src/main.js
+++ src/main.js
@@ -16,10 +16,10 @@ if(mock){
 }
 // 根据前端的跨域方式做调整 /a/b : /api/a/b => /a/b
 // axios.defaults.baseURL = 'https://www.easy-mock.com/mock/5dc7afee2b69d9223b633cbb/mimall';
+// axios.defaults.baseURL = env.baseURL; // 根据环境变量获取不同的请求地址
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000; 
-// 根据环境变量获取不同的请求地址
-// axios.defaults.baseURL = env.baseURL;
+
 // 接口错误拦截
 axios.interceptors.response.use(function(response){
   let res = response.data;
